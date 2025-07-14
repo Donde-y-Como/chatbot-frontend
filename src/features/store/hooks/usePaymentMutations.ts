db7a83286413f355d4ceb7bf8af7e3107e833dda
Carlos Sosa <78663955+carlosupreme@users.noreply.github.com>
2025-07-13 19:17:59 -0600

    Update usePaymentMutations.ts

--- src/features/store/hooks/usePaymentMutations.ts
+++ src/features/store/hooks/usePaymentMutations.ts
@@ -70,7 +70,7 @@ const addPaymentToOrder = async (data: AddPaymentToOrderData) => {
     type: 'sale' | 'order'
   }>(`/orders/${data.orderId}/payments`, {
     amount: data.amount,
-    paymentMethod: data.paymentMethod,
+    method: data.paymentMethod,
   })
 
   if (response.data.type === 'sale') {
